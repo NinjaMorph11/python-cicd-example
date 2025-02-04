81a70f1f3f5d84260bafca9008c2159211976dc3
Ninja11 <ninjamorph11@gmail.com>
2025-02-04 16:17:24 +0800

    Interesting

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='my-python-app', # Choose a name
+    version='0.1.0',
+    packages=find_packages(),
+    # ... other metadata (author, description, etc.)
+)
